9ab76b5c59fedf3c55eb94aa8352b1c54da9110b
Olga Donskikh <43335806+OlgaDonskikh-lab@users.noreply.github.com>
2025-04-30 17:13:00 +0000

    dbt deps - dependencies

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -7,7 +7,8 @@
 WITH src_reviews AS (
  SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+* FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
  AND review_date > (select max(review_date) from {{ this }})
